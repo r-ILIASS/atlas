df6c1103f3bb8724936da11858340b1ea0d99cb1
r-ILIASS <iliass.root@gmail.com>
2022-03-06 14:13:46 +0100

    feat: add search page - InfoCard component - loading Progressbar

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import {
   GlobeAltIcon,
@@ -12,15 +13,35 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 
-function Header() {
+function Header({ placeholder }) {
+  // State
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [numberOfGuests, setNumberOfGuests] = useState(1);
+  // Next Router
+  const router = useRouter();
 
+  // Handlers
   const handleSelect = (ranges) => {
     setStartDate(ranges.Selection.startDate);
     setEndDate(ranges.Selection.endDate);
+  };
+
+  const resetInput = (e) => {
+    setSearchInput("");
+  };
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numberOfGuests,
+      },
+    });
   };
 
   const selectionRange = {
@@ -32,13 +53,15 @@ function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
       {/* Left: Brand */}
-      <div className="relative flex h-5 cursor-pointer my-auto">
+      <div className="relative flex h-5 my-auto">
         <Image
           src="/logo.png"
           layout="fill"
           objectFit="contain"
           objectPosition="left"
           alt=""
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
         />
       </div>
 
@@ -49,22 +72,20 @@ function Header() {
           onChange={(e) => setSearchInput(e.target.value)}
           className="ml-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search..."
+          placeholder={placeholder || "Start your search.."}
         />
         <SearchIcon className="hidden md:inline-flex mx-2 h-8 bg-blue-400 p-2 cursor-pointer text-white rounded-full" />
       </div>
 
       {/* Right */}
-      <div className="flex space-x-4 items-center justify-end text-gray-500">
-        <p className="hidden md:inline cursor-pointer">Become a host</p>
-        <GlobeAltIcon className="h-6 cursor-pointer" />
+      <div className="flex items-center justify-end text-gray-500">
         <div className="flex space-x-2 items-center border-2 p-2 rounded-full">
           <MenuIcon className="h-6" />
           <UserCircleIcon className="h-6" />
         </div>
       </div>
 
-      {/* Search Control */}
+      {/* Search Controls */}
       {searchInput && (
         <div className="flex flex-col col-span-3 mx-auto mt-4">
           <DateRangePicker
@@ -89,10 +110,16 @@ function Header() {
             </div>
           </div>
           <div className="px-4 flex space-x-1">
-            <button className="flex-grow bg-red-400 py-1 rounded-md text-white">
+            <button
+              onClick={resetInput}
+              className="flex-grow bg-red-400 py-1 rounded-md text-white"
+            >
               Cancel
             </button>
-            <button className="flex-grow bg-blue-500 py-1 rounded-md font-semibold text-white">
+            <button
+              onClick={search}
+              className="flex-grow bg-blue-500 py-1 rounded-md font-semibold text-white"
+            >
               Search
             </button>
           </div>
---
+++ pages/search.js
@@ -0,0 +1,71 @@
+import { useRouter } from "next/router";
+import { format } from "date-fns";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
+
+function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, numberOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM");
+  const range = `${formattedStartDate} to ${formattedEndDate}`;
+
+  return (
+    <div>
+      <Header
+        placeholder={`${location} | ${range} | ${numberOfGuests} guests`}
+      />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ options <span className="text-red-400">from {range}</span> for{" "}
+            {numberOfGuests} guests
+          </p>
+
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+
+          <div className="hidden md:inline-flex space-x-2 mb-4 text-gray-800 whitespace-nowrap">
+            <p className="pill">Cancellation Flexibility</p>
+            <p className="pill">Type of Place</p>
+            <p className="pill">Price</p>
+            <p className="pill">Rooms</p>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults.map((item) => (
+              <InfoCard
+                key={item.img}
+                img={item.img}
+                title={item.title}
+                location={item.location}
+                description={item.description}
+                star={item.star}
+                price={item.price}
+                total={item.total}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
